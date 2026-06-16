1911e7cb1f41c91957e3cba0638d16f6a8778e64
Paul <pinchukpaul@gmail.com>
2026-06-15 18:30:59 -0600

    Add new func

--- gaps/utilities/__init__.py
+++ gaps/utilities/__init__.py
@@ -1,6 +1,6 @@
 """GAPs utilities"""
 
-from .base import recursively_update_dict, resolve_path
+from .base import recursively_update_dict, resolve_path, node_tag, TAG
 from .enums import CaseInsensitiveEnum
 from .io import (
     parse_points_input_to_df,
